06af0ade5f9d75ab1eca80ea447bc52943bd6acd
Elodie Ferrais <elodie.ferrais@gmail.com>
2014-02-14 16:09:40 -0800

    add testShouldReceiveManyMessage

--- TahoeSDKTests/KWMessageClientTests.m
+++ TahoeSDKTests/KWMessageClientTests.m
@@ -86,6 +86,66 @@
 
 }
 
+- (void)testShouldReceiveManyMessage {
+
+    KWMessageClient *messageClient = [[KWMessageClient alloc] init];
+    __block BOOL hasCalledBack = NO;
+
+    NSData *responseData = [self getDataFromJsonFile:@"TestDataMessageListMany"];
+
+    //Mock the http reponse
+    [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+        return YES; // Stub ALL requests without any condition
+    } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+        // Stub all those requests with Json responseData content
+        return [OHHTTPStubsResponse responseWithData:responseData statusCode:200 headers:@{@"Content-Type": @"text/json"}];
+    }];
+
+    [messageClient getMessagesForChannelIdentifier:@"test-channel" since:nil limit:0 completion:^(NSArray *messages, NSError *error) {
+        NSError *jsonError = nil;
+        NSArray *inputData = [[NSJSONSerialization JSONObjectWithData:responseData options:0 error:&jsonError] objectForKey:@"messages"];
+        XCTAssertNil(jsonError);
+        XCTAssertNotNil(inputData);
+
+        XCTAssertNotNil(messages);
+        XCTAssertNil(error);
+        XCTAssertTrue([inputData count] == [messages count]);
+
+        for (int i = 0; i < [inputData count]; i++) {
+            KWMessage *message = [messages objectAtIndex:i];
+            NSDictionary *messageJson = [inputData objectAtIndex:i];
+
+            //Message attributes
+            XCTAssertTrue([[messageJson objectForKey:@"id"] isEqualToString:message.identifier]);
+            XCTAssertTrue([[messageJson objectForKey:@"title"] isEqualToString:message.title]);
+
+            //Question attributes
+            XCTAssertTrue([message isKindOfClass:[KWQuestion class]]);
+            KWQuestion *question = (KWQuestion *)message;
+            XCTAssertNotNil(question.choices);
+            NSArray *choices = question.choices;
+            NSArray *choicesJson = [messageJson valueForKeyPath:@"data.choices"];
+            XCTAssertTrue(4 ==[question.choices count]);
+            XCTAssertTrue([[choicesJson objectAtIndex:0] isEqualToString: [choices objectAtIndex:0]]);
+            XCTAssertTrue([[choicesJson objectAtIndex:1] isEqualToString:[choices objectAtIndex:1]]);
+            XCTAssertTrue([[choicesJson objectAtIndex:2] isEqualToString:[choices objectAtIndex:2]]);
+            XCTAssertTrue([[choicesJson objectAtIndex:3] isEqualToString:[choices objectAtIndex:3]]);
+
+            hasCalledBack = YES;
+        }
+    }];
+
+    NSDate *loopUntil = [NSDate dateWithTimeIntervalSinceNow:300];
+    while (hasCalledBack == NO && [loopUntil timeIntervalSinceNow] > 0) {
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:loopUntil];
+    }
+
+    if (!hasCalledBack) {
+        XCTFail(@"Login with username and password timed out.");
+    }
+
+}
+
 -(NSData*) getDataFromJsonFile: (NSString *) fileName {
     NSBundle *myBundle = [NSBundle bundleForClass: [self class]];
     NSString *filePath = [myBundle pathForResource:fileName ofType:@"json"];
